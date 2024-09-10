6e436e7edac2b31a9c549197bf2ea32caf6b5188
AEscobarAngola <abrancho1908@gmail.com>
2024-09-10 19:50:07 +0200

    feat: creating de model for migration tasks

--- app/Models/Tasks.php
+++ app/Models/Tasks.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Tasks extends Model
 {
     use HasFactory;
+
+    protected $table ='tasks';
+
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
