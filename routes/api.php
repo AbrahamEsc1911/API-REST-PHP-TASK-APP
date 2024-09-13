85a5b90ff71f69a0c192cee44643e6042ec1c6ac
AEscobarAngola <abrancho1908@gmail.com>
2024-09-13 10:22:24 +0200

    update: small changes on routers, spaces between code lines

--- routes/api.php
+++ routes/api.php
@@ -3,27 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\usersController;
+use App\Http\Controllers\TaskController;
 
 // User CRUD
 Route::get('/users', [usersController::class, 'getAllUsers']);
-
 Route::get('/users/{id}', [usersController::class, 'getUserById']);
-
 Route::post('/register', [usersController::class, 'register'] );
-
 Route::post('/login', [usersController::class, 'login'] );
-
 Route::middleware('auth:sanctum')->post('/logout', [usersController::class, 'logout']);
-
 Route::put('/users/{id}', [usersController::class, 'updateUserData']);
-
 Route::delete('/users/{id?}', [usersController::class, 'deleteUser']);
 
 // TASKS CRUD
 
-Route::post('/tasks', function () {
-    return 'creating a new tasks';
-});
+Route::post('/tasks', []);
 
 Route::get('/tasks', function () {
     return 'getting all tasks';
